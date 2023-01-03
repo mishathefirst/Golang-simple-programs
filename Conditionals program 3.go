f9ecdf37845cbcff679b94877e6494895915eeb7
mishathefirst <30623681+mishathefirst@users.noreply.github.com>
2023-01-03 03:30:16 +0300

    Add files via upload

---
+++ Conditionals program 3.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a int
+    fmt.Scan(&a)
+    
+    if a < 10 {
+        fmt.Println(a)
+    } else if a < 100 {
+        fmt.Println(a / 10)
+    } else if a < 1000 {
+        fmt.Println(a / 100)
+    } else  if a < 10000 {
+        fmt.Println(a / 1000)
+    } else {
+        fmt.Println(1)
+    }
+}
+

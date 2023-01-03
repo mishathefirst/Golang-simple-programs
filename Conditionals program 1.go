b8d2cbac989db3631f6fe6d9a2ab430e184123bf
mishathefirst <30623681+mishathefirst@users.noreply.github.com>
2023-01-03 03:29:51 +0300

    Add files via upload

---
+++ Conditionals program 1.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a int
+        
+    fmt.Scan(&a)
+    switch {
+    case a > 0: 
+        fmt.Println("Число положительное")
+    case a < 0:
+        fmt.Println("Число отрицательное")
+    default: 
+        fmt.Println("Ноль")
+    }
+
+}
